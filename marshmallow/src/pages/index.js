2a7c4d757695cf801c4c88b62de59e8159797867
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-05-01 15:05:59 -0700

    Fixed Subscribe Form

--- marshmallow/src/pages/index.js
+++ marshmallow/src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Logo from '../../public/images/logo.png'
+import axios from 'axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,8 +14,30 @@ export default function Home() {
     email: ''
   })
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const post = await axios.post(`/api/NotionForm`, form)
+    if (post) alert("Successfully Submitted")
+
+    setForm({
+      firstName: '',
+      lastName: '',
+      email: ''
+    })
+    
+  }
+
   return (
-    <div className="min-h-screen overflow-y-auto flex justify-center items-center" style={{backgroundColor: "#4872B1"}}> 
+    <div className="min-h-screen overflow-y-auto flex justify-center items-center p-12" style={{backgroundColor: "#4872B1"}}> 
       <main style={{backgroundColor:"#4872B1"}}>
 
         <div className="my-15 flex justify-items-center flex-col items-center">
@@ -35,15 +58,15 @@ export default function Home() {
           </div>
 
           <div className="mt-20">
-          <form className="grid grid-cols-2 justify-center items-center mx-auto gap-5">
+          <form onSubmit={handleSubmit} className="grid grid-cols-2 justify-center items-center mx-auto gap-5">
             <label className='col-span-1 flex gap-x-4 flex-col'>
               <input 
               className="border-2 border-black-400 p-2 rounded-md" 
               type="text" 
               placeholder='First Name'
               required
-              name="first name"
-              
+              name="firstName"
+              onChange={handleChange}
               />
             
             </label>
@@ -54,8 +77,9 @@ export default function Home() {
               className="border-2 border-black-400 p-2 rounded-md" 
               type="text" 
               placeholder="Last Name"
-              name="last name"
+              name="lastName"
               required
+              onChange={handleChange}
               />
             
             </label>
@@ -67,10 +91,11 @@ export default function Home() {
               placeholder="Email Address"
               required
               name="email"
+              onChange={handleChange}
               />
             </label>
 
-            <button style={{backgroundColor: "#50BAF2"}} className=" col-span-2 text-white p-2 rounded-md ml-15 hover:bg-stone-400">Subscribe</button>
+            <button type="submit" style={{backgroundColor: "#50BAF2"}} className=" col-span-2 text-white p-2 rounded-md ml-15 hover:bg-stone-400">Subscribe</button>
 
           </form>
           </div>
